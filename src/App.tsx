a576f357b9760ccc5ca5b2a1932bfb77df0a2363
SiarheiLabodzin <siarhei.labodzin@innowise.com>
2024-11-27 12:00:29 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Input } from "npm-library-ui-kit-kat";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button xl color="white">
+        Add to Busket
+      </Button>
+      <br />
+
+      <Input xl label="input" placeholder="name" />
     </div>
   );
 }
